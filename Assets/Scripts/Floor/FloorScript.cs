2a66a77425c1fa5efbeec158c88e9e7cd6c7b50d
Pepi Dimi <pepi55@live.com>
2014-09-08 12:40:52 +0200

    added player

--- Assets/Scripts/Floor/FloorScript.cs
+++ Assets/Scripts/Floor/FloorScript.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class FloorScript : MonoBehaviour {
+	//publics
+	public int scrollSpeed = 5;
+
+	//privates
+	private Vector2 pos = new Vector2();
 
 	// Use this for initialization
 	void Start () {
-	
+		pos = new Vector2(this.transform.position.x, this.transform.position.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		pos.x -= scrollSpeed * Time.deltaTime;
+
+		if (this.transform.position.x <= -12.5f) {
+			pos.x += 25f;
+		}
+
+		this.transform.position = pos;
 	}
 }
